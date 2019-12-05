5ef80a5aeba2a527f7defe420e808d7ac9c1d691
Marcos Felipe <djmarcosban@hotmail.com>
2019-12-05 11:22:50 -0300

    instalacao instagram widget

--- index.php
+++ index.php
@@ -234,7 +234,9 @@
 				</p>
 			</div>
 			<div class="col-6">
-				Instagram aqui
+				<!-- SnapWidget -->
+				<script src="https://snapwidget.com/js/snapwidget.js"></script>
+				<iframe src="https://snapwidget.com/embed/767523" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden;  width:100%; "></iframe>
 			</div>
 		</div>
 	</div>
