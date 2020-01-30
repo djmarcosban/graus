8316f63c8326831e9bfa9e4ef17f47c7291b234d
Marcos Felipe <djmarcosban@hotmail.com>
2020-01-30 10:35:15 -0300

    instalacao captcha

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ define('SECRET_KEY','6Lf8NtQUAAAAAJfTMts4bzBq9XNTmU0d_uRZnxud');
 	})(window,document,'script','dataLayer','GTM-NTW837V');</script>
 	<!-- End Google Tag Manager -->
 	<title>Graus</title>
-	<!--<base href="https://graus.com.br/web/">-->
+	<base href="https://graus.com.br/web/">
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
